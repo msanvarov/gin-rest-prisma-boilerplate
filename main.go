74364c7f91580e40ad65ec77a7e2b5f383dc4227
msanvarov <salim.anvarov@yahoo.com>
2019-10-09 15:03:46 -0400

    :computer: server start

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/gin-rest-prisma-boilerplate/config"
+	"github.com/gin-rest-prisma-boilerplate/router"
+	"log"
 )
 
 func main() {
 	// set this when moving between production and development configs
 	yaml := config.Configuration("development")
-	fmt.Print(yaml.Get("server.port"))
+	port := yaml.GetString("server.port")
+
+	routing := router.Router()
+	fmt.Printf("🚀 Preparing to listen on port %s\n", port)
+	err := routing.Run(port)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
