eababe90b4a8d429b8f7a263bf2e9ed9853a06b1
msanvarov <salim.anvarov@yahoo.com>
2019-10-10 09:23:49 -0400

    :building_construction: reworked authentication controller with session management instead of basic auth

--- controllers/auth.go
+++ controllers/auth.go
@@ -2,7 +2,8 @@ package auth
 
 import (
 	"context"
-	"encoding/base64"
+	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-rest-prisma-boilerplate/db"
 	"github.com/gin-rest-prisma-boilerplate/forms"
@@ -10,7 +11,6 @@ import (
 	"github.com/gin-rest-prisma-boilerplate/utils"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type AuthenticationController struct{}
@@ -20,58 +20,113 @@ var (
 	contextB = context.Background()
 )
 
+func (AuthenticationController) GetSessionData(c *gin.Context) {
+	session := sessions.Default(c)
+	uuid := session.Get("uuid")
+	if uuid != nil {
+		c.JSON(http.StatusOK, gin.H{"uuid": uuid, "username": session.Get("username"),
+			"email": session.Get("email")})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to fetch session data. Make sure to be logged on."})
+		c.Abort()
+		return
+	}
+}
+
 func (AuthenticationController) Register(c *gin.Context) {
 	var registrationPayload forms.RegistrationForm
 	if validationErr := c.BindJSON(&registrationPayload); validationErr != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"operation": "Tried to bind request body to expected JSON fields.", "error": validationErr.Error(), "status": http.StatusBadRequest})
+		c.JSON(http.StatusBadRequest, gin.H{"operation": "Tried to bind request body to expected JSON fields.",
+			"error": validationErr.Error(), "status": http.StatusBadRequest})
+		c.Abort()
 		return
 	}
 	if hashedPass, hashErr := utils.EncryptPassword(registrationPayload.Password); hashErr != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"operation": "Tried to hash payload password.", "error": hashErr.Error(), "status": http.StatusInternalServerError})
+		c.JSON(http.StatusInternalServerError, gin.H{"operation": "Tried to hash payload password.",
+			"error": hashErr.Error(), "status": http.StatusInternalServerError})
+		c.Abort()
 		return
 	} else {
 		registrationPayload.Password = hashedPass
 	}
 
-	user, err := client.CreateUser(prisma.UserCreateInput{
-		Email: registrationPayload.Email,
-		Name: registrationPayload.Name,
+	user, prismaError := client.CreateUser(prisma.UserCreateInput{
+		Email:    registrationPayload.Email,
+		Name:     registrationPayload.Name,
 		Username: registrationPayload.Username,
 		Password: registrationPayload.Password,
 	}, ).Exec(contextB)
 
-	if err != nil {
-		log.Fatal(err)
+	if prismaError != nil {
+		log.Println(prismaError)
+		c.JSON(http.StatusNotAcceptable, gin.H{"message":
+		"Failed to create the account. Please try another username."})
+		c.Abort()
+		return
+	}
+	// setting session keys
+	session := sessions.Default(c)
+	session.Set("uuid", user.ID)
+	session.Set("email", user.Email)
+	session.Set("username", user.Username)
+
+	if sessionError := session.Save(); sessionError != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message":
+		"Failed to set session keys. Please try to register again."})
+		c.Abort()
+		log.Println(sessionError)
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"user": user,
+		"message": "Registered.",
+		"user":    user,
 	})
 }
 
-func BasicAuth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authH := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
+func (AuthenticationController) Login(c *gin.Context) {
+	var loginPayload forms.LoginForm
+	if validationErr := c.BindJSON(&loginPayload); validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"operation": "Tried to bind request body to expected JSON fields.",
+			"error": validationErr.Error(), "status": http.StatusBadRequest})
+		c.Abort()
+		return
+	}
 
-		if len(authH) != 2 || authH[0] != "Basic" {
-			GlobalErrorHandler(c, http.StatusUnauthorized, "Unauthorized")
-			return
+	if user, err := client.User(
+		prisma.UserWhereUniqueInput{Username: &loginPayload.Username}).Exec(contextB); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message":
+		fmt.Sprintf("The profile with the username: %s doesn't exist. Please register before trying to login.",
+			loginPayload.Username)})
+		log.Println(err)
+	} else {
+		if passwordMatch := utils.CheckPassword(loginPayload.Password, user.Password); passwordMatch != true {
+			c.JSON(http.StatusNotAcceptable, gin.H{"message": "Invalid password details. Please try again."})
+		} else {
+			// setting session keys
+			session := sessions.Default(c)
+			session.Set("uuid", user.ID)
+			session.Set("email", user.Email)
+			session.Set("username", user.Name)
+			if sessionError := session.Save(); sessionError != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to logout. Please try again."})
+				c.Abort()
+				log.Println(sessionError)
+			}
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Logged in.",
+				"user":    user,
+			})
 		}
-		payload, _ := base64.StdEncoding.DecodeString(authH[1])
-		pair := strings.SplitN(string(payload), ":", 2)
-
-		if len(pair) != 2 || !authenticateUser(pair[0], pair[1]) {
-			GlobalErrorHandler(c,http.StatusUnauthorized, "Unauthorized")
-			return
-		}
-		c.Next()
 	}
 }
 
-func authenticateUser(username, password string) bool {
-	if user, err := client.User(prisma.UserWhereUniqueInput{Username: &username}).Exec(contextB); err != nil {
-		log.Fatal(err)
-		return false
+func (AuthenticationController) Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	if sessionError := session.Save(); sessionError != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to logout. Please try again."})
+		c.Abort()
+		log.Println(sessionError)
 	} else {
-		return utils.CheckPassword(password, user.Password)
+		c.JSON(http.StatusOK, gin.H{"message": "Logged out..."})
 	}
 }
