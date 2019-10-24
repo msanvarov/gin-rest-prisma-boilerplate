30c27f2521b3131afd85ba19c8993cdeaf0fc75b
msanvarov <salim.anvarov@yahoo.com>
2019-10-24 18:05:50 -0400

    :lipstick: cleaned up files

--- forms/user.go
+++ forms/user.go
@@ -2,12 +2,13 @@ package forms
 
 // Registration Form
 type RegistrationForm struct {
-	Name string `json:"name" binding:"required,max=60"`
+	Name     string `json:"name" binding:"required,max=60"`
 	Username string `json:"username" binding:"required,max=20"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
 
+// Login Form
 type LoginForm struct {
 	Username string `json:"username" binding:"required,max=20"`
 	Password string `json:"password" binding:"required"`
