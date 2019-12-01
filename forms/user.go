8222884a77deb8de8e4550495c1dbaaa5785f17d
msanvarov <salim.anvarov@yahoo.com>
2019-11-30 22:27:38 -0500

    :pencil2: examples for swagger

--- forms/user.go
+++ forms/user.go
@@ -2,14 +2,14 @@ package forms
 
 // RegistrationForm structure for defining the type of payload to valid against when registering.
 type RegistrationForm struct {
-	Name     string `json:"name" binding:"required,max=60"`
-	Username string `json:"username" binding:"required,max=20"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Name     string `json:"name" binding:"required,max=60" example:"account name"`
+	Username string `json:"username" binding:"required,max=20" example:"account_mock"`
+	Email    string `json:"email" binding:"required,email" example:"account@mock.com"`
+	Password string `json:"password" binding:"required" example:"password"`
 }
 
 // LoginForm structure for defining the type of payload to valid against when logging in.
 type LoginForm struct {
-	Username string `json:"username" binding:"required,max=20"`
-	Password string `json:"password" binding:"required"`
+	Username string `json:"username" binding:"required,max=20" example:"account_mock"`
+	Password string `json:"password" binding:"required" example:"password"`
 }
