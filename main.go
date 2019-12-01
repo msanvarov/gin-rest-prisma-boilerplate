32ef8b8a2eae412c004e5d24cdb466ed99102fdf
msanvarov <salim.anvarov@yahoo.com>
2019-11-30 22:32:40 -0500

    :pencil: added swagger base information

--- main.go
+++ main.go
@@ -8,11 +8,25 @@ import (
 	"github.com/msanvarov/gin-rest-prisma-boilerplate/router"
 )
 
+// @title API
+// @version 1.0
+// @description Simple API
+
+// @contact.name API Support
+// @contact.url https://msanvarov.github.io/personal-portfolio/
+// @contact.email msalanvarov@gmail.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:9000
+// @basepath /
+
 // Main method to act as a starting point for the web server.
 func main() {
-	yaml := config.Configuration("config")
+	config.Configure("config")
+	yaml := config.GetConfiguration()
 	port := yaml.GetString("server.port")
-
 	routing := router.Router(yaml)
 	fmt.Printf("🚀 Preparing to listen on port %s\n", port)
 	err := routing.Run(port)
